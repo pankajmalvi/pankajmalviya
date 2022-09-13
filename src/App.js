85e0a0b32155fee8a4379916eb0b30904b0c88d7
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-09-13 12:38:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const data = require('./data.json')
+
+
+
+function App() {
+  console.log(data.name);
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;

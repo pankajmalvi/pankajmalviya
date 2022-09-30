80cb74f086b37491045cc6d72a2890fb67390244
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-09-30 15:34:16 +0530

    Added Basic Structure of web page in App.js just for reference

--- src/App.js
+++ src/App.js
@@ -9,7 +9,51 @@ function App() {
   let resumeLink = data["personal-details"]["resume-link"]
   return (
     <div className="App">
-      <Navbar resumeLink={resumeLink} />
+      <header>
+        <Navbar resumeLink={resumeLink} />
+      </header>
+      <main>
+        <hero>
+          {/* Greeting
+          Name
+          Tagline
+          Profile Description
+          Resume Link Can be included not necessary */}
+        </hero>
+
+        <section id="section-about">
+          {/* About Heading
+           About article  */}
+        </section>
+
+        <section id="section-job">
+          {/* Link Headings
+           Work > Works */}
+        </section>
+
+        <section id="section-project">
+          {/* Link Headings
+           Project Description > Project */}
+        </section>
+
+        <section id="section-contact">
+          {/* Link Heading Type 2
+          Description
+          Contact Button */}
+        </section>
+
+        <article id="left-side-links">
+          {/* Github
+          LinkedIn
+          Other links */}
+        </article>
+        <article id="right-side-mail">
+          {/* Mail Id */}
+        </article>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }

5ab2646ef6fa4112f64d08d126912cac9d79af6c
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-10-06 13:17:11 +0530

    Added Assets and changes in Section Heading

---
+++ src/components/sections/SectionHeading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SectionHeading({ heading }) {
+    return (
+        <h2 className='section-heading section-heading-numbered'>{heading}</h2>
+
+    )
+}
+
+export default SectionHeading

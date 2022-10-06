ad95bf203de575fe85d792a0e6e68a9213d60896
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-10-06 13:11:49 +0530

    Navbar scroll animation changes

--- src/components/sections/navbar/Navbar.js
+++ src/components/sections/navbar/Navbar.js
@@ -1,9 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Logo from './Logo'
 import Navigation from './Navigation'
 import './styles/Navbar.scss'
 
 export class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            prevScrollpos: window.pageYOffset,
+            visible: true
+        };
+    }
+
+    // Adds an event listener when the component is mount.
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    // Remove the event listener when the component is unmount.
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    // Hide or show the menu.
+    handleScroll = () => {
+        const { prevScrollpos } = this.state;
+
+        const currentScrollPos = window.pageYOffset;
+        const visible = prevScrollpos > currentScrollPos;
+
+        this.setState({
+            prevScrollpos: currentScrollPos,
+            visible
+        });
+    };
+
 
     links = [
         "About",
@@ -13,12 +46,13 @@ export class Navbar extends Component {
     ]
     render() {
         return (
-            <nav className="navbar">
+            <nav className={` ${!this.state.visible ? "navbar--hidden" : "navbar"}`}>
                 <Logo />
                 <Navigation links={this.links} resumeLink={this.props.resumeLink} />
             </nav>
         )
     }
 }
+
 
 export default Navbar

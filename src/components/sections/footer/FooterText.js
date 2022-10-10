afeb45cb5b3970ab4ccd682c647f8ccd5643f913
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-10-10 13:39:30 +0530

    Added Attribution for Design Inspiration

--- src/components/sections/footer/FooterText.js
+++ src/components/sections/footer/FooterText.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 function FooterText() {
+    const style = {
+        color: 'var(--green)'
+    }
     return (
-        <p>Built by Pankaj Malviya</p>
+        <p>Built by <span style={{ color: 'var(--green)' }}>Pankaj Malviya</span> |
+            Design inspired by <a style={style} href="http://brittanychiang.com" target='_blank' rel="noreferrer">Brittany Chiang</a></p>
     )
 }
 

9efff854736ef61800a18af594338f22f42d7706
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-10-10 13:11:11 +0530

    Added Show More and Show Less functionality in Projects section

--- src/components/sections/projects/Projects.js
+++ src/components/sections/projects/Projects.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 import SectionHeading from '../../common/SectionHeading'
 import Project from './Project'
 import './styles/Projects.scss'
 
 function Projects({ projects }) {
+
+    const [showMore, setShowMore] = useState(true)
     return (
         <section id="work" className='section-project'>
             <SectionHeading heading={"Some Things I've Built"} />
             <ul className="projects">
-                {projects.map((project, idx) =>
-                    <Project project={project} key={idx} />
-                )}
+                {
+                    showMore ?
+                        projects.slice(0, 6).map((project, idx) => <Project project={project} key={idx} />
+                        )
+                        :
+                        projects.map((project, idx) => <Project project={project} key={idx} />
+                        )
+                }
             </ul>
+            <div className="project-button">
+
+                <button className='project-button' onClick={() => setShowMore(!showMore)}>{showMore ? 'Show More' : 'Show Less'}</button>
+            </div>
         </section>
     )
 }
